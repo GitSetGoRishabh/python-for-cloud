745d535d326bc91de1e4844b85a0de4dbfaae9b4
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-01-21 10:15:56 +0530

    coniditonals in python

---
+++ basics/conditionals.py
@@ -0,0 +1,7 @@
+dayOfWeek=input("Enter the day of week : ").lower()
+print(dayOfWeek)
+
+if dayOfWeek=="saturday" or dayOfWeek=="sunday":
+    print("Work harder boy!")
+else:
+    print("Practice")
